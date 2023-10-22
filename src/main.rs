a0c1f13aaed456725f6081cceb20a096aa7ab628
kxxt <rsworktech@outlook.com>
2023-10-22 23:12:28 +0800

    add unimplemented

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,9 @@ fn main() -> color_eyre::Result<()> {
             tracer::Tracer::new(tracing_args)
                 .start_root_process(cmd.into_iter().map(|x| CString::new(x).unwrap()).collect())?;
         }
-        CliCommand::Tree { cmd, tracing_args } => {}
+        CliCommand::Tree { cmd, tracing_args } => {
+            unimplemented!("tree mode not implemented yet")
+        }
     }
     Ok(())
 }
