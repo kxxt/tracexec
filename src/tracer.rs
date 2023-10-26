d4c1205b1350264ededc1306e9a1dd4eaf81e5f2
kxxt <rsworktech@outlook.com>
2023-10-26 15:12:07 +0800

    fix: don't use ptrace GETREGSET on x86_64

--- src/tracer.rs
+++ src/tracer.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, ffi::CString, path::PathBuf, process::exit};
 
+use cfg_if::cfg_if;
 use nix::{
     errno::Errno,
     libc::{pid_t, raise, SYS_clone, SYS_clone3, AT_EMPTY_PATH, SIGSTOP},
@@ -48,27 +49,36 @@ fn ptrace_syscall(pid: Pid) -> Result<(), Errno> {
 }
 
 fn ptrace_getregs(pid: Pid) -> Result<PtraceRegisters, Errno> {
-    let mut regs = std::mem::MaybeUninit::<PtraceRegisters>::uninit();
-    let iovec = nix::libc::iovec {
-        iov_base: regs.as_mut_ptr() as AddressType,
-        iov_len: std::mem::size_of::<PtraceRegisters>(),
-    };
-    let ptrace_result = unsafe {
-        nix::libc::ptrace(
-            nix::libc::PTRACE_GETREGSET,
-            pid.as_raw(),
-            nix::libc::NT_PRSTATUS,
-            &iovec as *const _ as *const nix::libc::c_void,
-        )
-    };
-    let regs = if -1 == ptrace_result {
-        let errno = nix::errno::Errno::last();
-        return Err(errno);
-    } else {
-        assert_eq!(iovec.iov_len, std::mem::size_of::<PtraceRegisters>());
-        unsafe { regs.assume_init() }
-    };
-    Ok(regs)
+    // Don't use GETREGSET on x86_64.
+    // In some cases(it usually happens several times at and after exec syscall exit),
+    // we only got 68/216 bytes into `regs`, which seems unreasonable. Not sure why.
+    cfg_if! {
+        if #[cfg(target_arch = "x86_64")] {
+            ptrace::getregs(pid)
+        } else {
+            let mut regs = std::mem::MaybeUninit::<PtraceRegisters>::uninit();
+            let iovec = nix::libc::iovec {
+                iov_base: regs.as_mut_ptr() as AddressType,
+                iov_len: std::mem::size_of::<PtraceRegisters>(),
+            };
+            let ptrace_result = unsafe {
+                nix::libc::ptrace(
+                    nix::libc::PTRACE_GETREGSET,
+                    pid.as_raw(),
+                    nix::libc::NT_PRSTATUS,
+                    &iovec as *const _ as *const nix::libc::c_void,
+                )
+            };
+            let regs = if -1 == ptrace_result {
+                let errno = nix::errno::Errno::last();
+                return Err(errno);
+            } else {
+                assert_eq!(iovec.iov_len, std::mem::size_of::<PtraceRegisters>());
+                unsafe { regs.assume_init() }
+            };
+            Ok(regs)
+        }
+    }
 }
 
 impl Tracer {
