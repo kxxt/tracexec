aab6cd957684aad814d7fcee18686c06ebbc9f19
kxxt <rsworktech@outlook.com>
2023-10-27 19:15:25 +0800

    feat: riscv64 support

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -7,6 +7,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "aarch64")] {
         pub mod aarch64;
         pub(crate) use aarch64::*;
+    } else if #[cfg(target_arch = "riscv64")] {
+        pub mod riscv64;
+        pub(crate) use riscv64::*;
     } else {
         compile_error!("unsupported architecture");
     }
