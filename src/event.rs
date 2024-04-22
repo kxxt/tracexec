b60d4c22ee21b8546dac4a6c32e969eb1256d8b1
kxxt <rsworktech@outlook.com>
2024-04-22 22:12:32 +0800

    refactor: add more macros

--- src/event.rs
+++ src/event.rs
@@ -1,4 +1,5 @@
 use crossterm::event::KeyEvent;
+use itertools::chain;
 use nix::{sys::signal::Signal, unistd::Pid};
 use ratatui::{
     style::{Color, Stylize},
@@ -44,6 +45,18 @@ pub enum Action {
     PrevItem,
 }
 
+macro_rules! tracer_event_spans {
+    ($pid: expr, $comm: expr, $printer_args: expr, $($t:tt)*) => {
+        chain!([
+            Some($pid.to_string().fg(Color::Yellow)),
+            $printer_args
+                .trace_comm
+                .then_some(format!("<{}>", $comm).fg(Color::Cyan)),
+            Some(": ".into()),
+        ], [$($t)*])
+    };
+}
+
 impl TracerEvent {
     pub fn to_tui_line(&self, args: &PrinterArgs) -> Line {
         match self {
@@ -52,14 +65,13 @@ impl TracerEvent {
             TracerEvent::Error => "Error".into(),
             TracerEvent::FatalError => "FatalError".into(),
             TracerEvent::NewChild { ppid, pcomm, pid } => {
-                let spans = [
-                    Some(ppid.to_string().fg(Color::Yellow)),
-                    args.trace_comm
-                        .then_some(format!("<{}>", pcomm).fg(Color::Cyan)),
-                    Some(": ".into()),
+                let spans = tracer_event_spans!(
+                    ppid,
+                    pcomm,
+                    args,
                     Some("new child ".fg(Color::Magenta)),
                     Some(pid.to_string().fg(Color::Yellow)),
-                ];
+                );
                 spans.into_iter().flatten().collect()
             }
             TracerEvent::Exec => "Exec".into(),
