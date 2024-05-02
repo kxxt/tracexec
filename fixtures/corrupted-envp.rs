72ceeb1ac8deb41aa43ec1b085f9dfcdae3570b7
kxxt <rsworktech@outlook.com>
2024-05-02 11:59:16 +0800

    test: add an executable that exec with bad envp

---
+++ fixtures/corrupted-envp.rs
@@ -0,0 +1,20 @@
+use nix::libc::execve;
+
+fn main() {
+  let i = std::env::args()
+    .nth(0)
+    .unwrap_or_else(|| "0".to_string())
+    .parse()
+    .unwrap_or(0);
+  if i > 3 {
+    return;
+  }
+  let arg0 = format!("{}\0", i + 1);
+  unsafe {
+    execve(
+      "/proc/self/exe\0".as_ptr().cast(),
+      [arg0.as_ptr() as *const nix::libc::c_char, std::ptr::null()].as_ptr(),
+      b"asfdasfafadfasdfgsadfg".as_ptr().cast(),
+    );
+  }
+}
