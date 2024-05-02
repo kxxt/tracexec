6886a86caf779076a5eb2c87f137c78398351379
kxxt <rsworktech@outlook.com>
2024-05-02 12:07:47 +0800

    opt/inspect: use into_owned instead of to_string

--- src/inspect.rs
+++ src/inspect.rs
@@ -44,7 +44,7 @@ pub fn read_pathbuf(pid: Pid, address: AddressType) -> color_eyre::Result<PathBu
 
 pub fn read_string(pid: Pid, address: AddressType) -> color_eyre::Result<String> {
   // Waiting on https://github.com/rust-lang/libs-team/issues/116
-  read_generic_string(pid, address, |x| String::from_utf8_lossy(&x).to_string())
+  read_generic_string(pid, address, |x| String::from_utf8_lossy(&x).into_owned())
 }
 
 pub fn read_null_ended_array<TItem>(
