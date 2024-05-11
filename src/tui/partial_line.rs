c49c240379f770603610a96c2d01bea317b0b544
kxxt <rsworktech@outlook.com>
2024-05-12 00:55:24 +0800

    fix/tui: handle graphemes for partial_line

--- src/tui/partial_line.rs
+++ src/tui/partial_line.rs
@@ -1,13 +1,15 @@
 use std::borrow::Cow;
 
 use ratatui::text::Line;
+use unicode_segmentation::UnicodeSegmentation;
+use unicode_width::UnicodeWidthStr;
 
 pub trait PartialLine<'a> {
   fn substring(self, start: usize, len: u16) -> Line<'a>;
 }
 
 impl<'a> PartialLine<'a> for Line<'a> {
-  // FIXME: Unicode is hard
+  // Unicode is hard
   fn substring(mut self, start: usize, len: u16) -> Line<'a> {
     let len = len as usize;
     let end = start + len;
@@ -20,37 +22,63 @@ impl<'a> PartialLine<'a> for Line<'a> {
     let mut discard_until = 0;
     let mut discard_after = self.spans.len();
     for (i, span) in self.spans.iter_mut().enumerate() {
-      let span_len = span.content.len();
-      if cur + span_len < start {
-        cur += span_len;
+      let span_width = span.width();
+      if cur + span_width < start {
+        // Discard this hidden span
+        cur += span_width;
         discard_until = i + 1;
         continue;
       }
       if cur >= end {
+        // Discard all following spans because we have already covered all visible spans
         discard_after = i;
         break;
       }
+      // The start and end defined by width
       let start = if cur < start { start - cur } else { 0 };
-      let end = if cur + span_len > end {
+      let end = if cur + span_width > end {
         end - cur
       } else {
-        span_len
+        span_width
       };
-      match span.content {
-        Cow::Borrowed(s) => {
-          span.content = Cow::Borrowed(&s[start..end]);
-        }
-        Cow::Owned(ref mut s) => {
-          s.drain(end..);
-          s.drain(..start);
+      let mut start_index = 0;
+      let mut end_index = span.content.len(); // exclusive
+      let mut cum = 0; // cumulative width
+      for (idx, grapheme) in span.content.grapheme_indices(true) {
+        let grapheme_width = grapheme.width();
+        if cum + grapheme_width < start {
+          start_index = idx + grapheme.len();
+          cum += grapheme_width;
+        } else if start != 0 {
+          // Skip the grapheme that doesn't fit in the start
+          start_index = idx + grapheme.len();
+          break;
         }
       }
-      cur += span_len;
+      cum = span_width;
+      for (idx, grapheme) in span.content.grapheme_indices(true).rev() {
+        let grapheme_width = grapheme.width();
+        if cum - grapheme_width >= end {
+          end_index = idx;
+          cum -= grapheme_width;
+        } else {
+          break;
+        }
+      }
+      match span.content {
+        Cow::Borrowed(s) => {
+          span.content = Cow::Borrowed(&s[start_index..end_index]);
+        }
+        Cow::Owned(ref mut s) => {
+          s.drain(end_index..);
+          s.drain(..start_index);
+        }
+      }
+      cur += span_width;
     }
     self.spans.drain(discard_after..);
     self.spans.drain(..discard_until);
 
     self
   }
-
 }
