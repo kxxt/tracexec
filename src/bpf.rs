dba48a94345447ed8d555696b6666d6a617e2dc1
kxxt <rsworktech@outlook.com>
2024-08-12 13:01:10 +0800

    feat/bpf: print argv and envp in userspace

--- src/bpf.rs
+++ src/bpf.rs
@@ -1,5 +1,13 @@
-use std::{mem::MaybeUninit, time::Duration};
+use std::{
+  borrow::Cow,
+  collections::HashMap,
+  ffi::CStr,
+  mem::MaybeUninit,
+  sync::{Arc, OnceLock, RwLock},
+  time::Duration,
+};
 
+use arcstr::ArcStr;
 use color_eyre::eyre::bail;
 use libbpf_rs::{
   num_possible_cpus,
@@ -8,6 +16,8 @@ use libbpf_rs::{
 };
 use nix::libc;
 use skel::types::{event_header, event_type, exec_event};
+
+use crate::cache::StringCache;
 
 pub mod skel {
   include!(concat!(
@@ -41,26 +51,49 @@ pub fn experiment() -> color_eyre::Result<()> {
   skel.attach()?;
   let events = skel.maps.events;
   let mut builder = RingBufferBuilder::new();
-  builder.add(&events, |data| {
-    assert!(data.len() > size_of::<event_header>());
+  let strings: Arc<RwLock<HashMap<u64, Vec<(ArcStr, u32)>>>> =
+    Arc::new(RwLock::new(HashMap::new()));
+  builder.add(&events, move |data| {
+    assert!(data.len() > size_of::<event_header>(), "data too short: {data:?}");
     let header: event_header = unsafe { std::ptr::read(data.as_ptr() as *const _) };
     match unsafe { header.r#type.assume_init() } {
       event_type::SYSENTER_EVENT => unreachable!(),
       event_type::SYSEXIT_EVENT => {
         assert_eq!(data.len(), size_of::<exec_event>());
         let event: exec_event = unsafe { std::ptr::read(data.as_ptr() as *const _) };
-        eprintln!(
-          "Exec event: {} exec {} argc {} envc {}",
+        eprint!(
+          "{} exec {} argv ",
           String::from_utf8_lossy(&event.comm),
           String::from_utf8_lossy(&event.base_filename),
-          event.count[0],
-          event.count[1]
         );
+        for i in 0..event.count[0] {
+          eprint!(
+            "{:?} ",
+            strings.read().unwrap().get(&event.header.eid).unwrap()[i as usize].0
+          );
+        }
+        eprint!("envp ");
+        for i in event.count[0]..(event.count[0] + event.count[1]) {
+          eprint!(
+            "{:?} ",
+            strings.read().unwrap().get(&event.header.eid).unwrap()[i as usize].0
+          );
+        }
+        eprintln!("= {}", event.ret);
       }
       event_type::STRING_EVENT => {
         let header_len = size_of::<event_header>();
-        let string = String::from_utf8_lossy(&data[header_len..]);
-        eprintln!("String for EID: {}: {}", header.eid, string);
+        let header: event_header = unsafe { std::ptr::read(data.as_ptr() as *const _) };
+        let string = String::from_utf8_lossy(
+          CStr::from_bytes_with_nul(&data[header_len..])
+            .unwrap()
+            .to_bytes(),
+        );
+        let cached = cached_cow(string);
+        let mut lock_guard = strings.write().unwrap();
+        let strings = lock_guard.entry(header.eid).or_default();
+        strings.push((cached, header.flags));
+        drop(lock_guard);
       }
       event_type::FD_EVENT => todo!(),
     }
@@ -71,3 +104,13 @@ pub fn experiment() -> color_eyre::Result<()> {
     rb.poll(Duration::from_millis(1000))?;
   }
 }
+
+fn cached_cow(cow: Cow<str>) -> ArcStr {
+  let cache = CACHE.get_or_init(|| Arc::new(RwLock::new(StringCache::new())));
+  match cow {
+    Cow::Borrowed(s) => cache.write().unwrap().get_or_insert(s),
+    Cow::Owned(s) => cache.write().unwrap().get_or_insert_owned(s),
+  }
+}
+
+static CACHE: OnceLock<Arc<RwLock<StringCache>>> = OnceLock::new();
