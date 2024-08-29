cdf25d49d96b1b933c2ed5d3edbba3a329da3454
kxxt <rsworktech@outlook.com>
2024-08-29 09:25:28 +0800

    fix: make cross-compilation work

--- src/bpf/tracexec_system.bpf.c
+++ src/bpf/tracexec_system.bpf.c
@@ -3,7 +3,7 @@
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-#include <errno.h>
+#include <asm-generic/errno.h>
 
 char LICENSE[] SEC("license") = "GPL";
 
