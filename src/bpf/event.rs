1d8c57050ebdf9248e4bb7eb689f20466881f9df
kxxt <rsworktech@outlook.com>
2024-08-31 16:01:21 +0800

    refacto/bpf: store temporary info in EventStorage

--- src/bpf/event.rs
+++ src/bpf/event.rs
@@ -6,12 +6,14 @@ use crate::{event::OutputMsg, proc::FileDescriptorInfoCollection};
 
 /// The temporary storage for receiving information
 /// about an event from the BPF ringbuf
+#[derive(Debug, Default)]
 pub struct EventStorage {
-  strings: Vec<(ArcStr, u32)>,
-  fdinfo_map: FileDescriptorInfoCollection,
-  pathes: HashMap<i32, Path>,
+  pub strings: Vec<(ArcStr, u32)>,
+  pub fdinfo_map: FileDescriptorInfoCollection,
+  pub paths: HashMap<i32, Path>,
 }
 
+#[derive(Debug)]
 pub struct Path {
   // Used to avoid prefixing
   // paths from synthetic filesystems
