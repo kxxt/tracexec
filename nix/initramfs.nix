b38fbe5c9765310378d5cba118a6d9a95771c7d3
Levi Zim <rsworktech@outlook.com>
2024-10-26 23:28:47 +0800

    nix: build kernel and initrd

---
+++ nix/initramfs.nix
@@ -0,0 +1,139 @@
+# Credits: https://github.com/jordanisaacs/kernel-module-flake
+# Original Copyright Notice:
+
+# MIT License
+
+# Copyright (c) 2022 Jordan Isaacs
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+{ lib
+, buildEnv
+, writeScript
+, makeInitrdNG
+, bash
+, busybox
+, kmod
+, dropbear
+,
+}: { kernel
+   , modules ? [ ]
+   , extraBin ? { }
+   , extraContent ? { }
+   , storePaths ? [ ]
+   , extraInit ? ""
+   ,
+   }:
+let
+  busyboxStatic = busybox.override { enableStatic = true; };
+
+  initrdBinEnv = buildEnv {
+    name = "initrd-emergency-env";
+    paths = map lib.getBin initrdBin;
+    pathsToLink = [ "/bin" "/sbin" ];
+    postBuild = lib.concatStringsSep "\n" (lib.mapAttrsToList (n: v: "ln -s ${v} $out/bin/${n}") extraBin);
+  };
+
+  moduleEnv = buildEnv {
+    name = "initrd-modules";
+    paths = modules;
+    pathsToLink = [ "/lib/modules/${kernel.modDirVersion}/misc" ];
+  };
+
+  content =
+    {
+      "/bin" = "${initrdBinEnv}/bin";
+      "/sbin" = "${initrdBinEnv}/sbin";
+      "/init" = init;
+      "/modules" = "${moduleEnv}/lib/modules/${kernel.modDirVersion}/misc";
+    }
+    // extraContent;
+
+  initrdBin = [ bash busyboxStatic kmod dropbear ];
+
+  initialRamdisk = makeInitrdNG {
+    compressor = "gzip";
+    strip = false;
+    contents =
+      map
+        (path: {
+          source = path;
+        })
+        storePaths
+      ++ lib.mapAttrsToList
+        (n: v: {
+          source = v;
+          target = n;
+        })
+        content;
+  };
+
+  init = writeScript "init" ''
+    #!/bin/sh
+
+    export PATH=/bin/
+
+    mkdir -p /proc
+    mkdir -p /sys
+    mkdir -p /dev
+    mount -t devtmpfs none /dev
+    mkdir -p /dev/pts
+    mount -t devpts none /dev/pts
+    mount -t proc none /proc
+    mount -t sysfs none /sys
+    mount -t debugfs debugfs /sys/kernel/debug
+
+    mkdir -p /etc/dropbear
+    echo /bin/bash > /etc/shells
+    cat > /etc/passwd << "EOF"
+    root::0:0:root:/root:/bin/bash
+    EOF
+    passwd -d root
+
+    ifconfig lo 127.0.0.1
+    ifconfig eth0 10.0.2.15
+    ip route add default via 10.0.2.2
+
+    mkdir -p /run/booted-system/kernel-modules/lib/modules/${kernel.modDirVersion}/build
+    tar -xf /sys/kernel/kheaders.tar.xz -C /run/booted-system/kernel-modules/lib/modules/${kernel.modDirVersion}/build
+
+    dropbear -F -R -E -B &
+
+    ${extraInit}
+
+    cat <<!
+
+    Boot took $(cut -d' ' -f1 /proc/uptime) seconds
+
+            _       _     __ _
+      /\/\ (_)_ __ (_)   / /(_)_ __  _   ___  __
+     /    \| | '_ \| |  / / | | '_ \| | | \ \/ /
+    / /\/\ \ | | | | | / /__| | | | | |_| |>  <
+    \/    \/_|_| |_|_| \____/_|_| |_|\__,_/_/\_\
+
+    Welcome to mini_linux
+
+
+    !
+
+    # Get a new session to allow for job control and ctrl-* support
+    exec setsid -c /bin/sh
+  '';
+in
+initialRamdisk
---
+++ nix/kernel-source.nix
@@ -0,0 +1,182 @@
+# Credits: https://github.com/jordanisaacs/kernel-module-flake
+# Original Copyright Notice:
+
+# MIT License
+
+# Copyright (c) 2022 Jordan Isaacs
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+{ pkgs
+, lib ? pkgs.lib
+, enableGdb
+,
+}:
+let
+  version = "6.6.58";
+  localVersion = "-ukci";
+in
+{
+  kernelArgs = {
+    inherit enableGdb;
+
+    inherit version;
+    src =
+      pkgs.fetchurl {
+        url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+        sha256 = "sha256-59+B5YjXD6tew+w7sErFPVHwhg/DsexF4KQWegJomds=";
+      };
+
+    # Add kernel patches here
+    kernelPatches =
+      let
+        fetchSet = lib.imap1 (i: hash: {
+          # name = " "kbuild-v${builtins.toString i}";
+          patch = pkgs.fetchpatch {
+            inherit hash;
+            url = "https://lore.kernel.org/rust-for-linux/20230109204520.539080-${builtins.toString i}-ojeda@kernel.org/raw";
+          };
+        });
+
+        patches = fetchSet [
+        ];
+      in
+      patches;
+
+    inherit localVersion;
+    modDirVersion = version + localVersion;
+  };
+  kernelConfig = {
+    # See https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/system/boot/kernel_config.nix
+    structuredExtraConfig = with lib.kernel;
+      {
+        DEBUG_FS = yes;
+        DEBUG_KERNEL = yes;
+        DEBUG_MISC = yes;
+        DEBUG_BUGVERBOSE = yes;
+        DEBUG_BOOT_PARAMS = yes;
+        DEBUG_STACK_USAGE = yes;
+        DEBUG_SHIRQ = yes;
+        DEBUG_ATOMIC_SLEEP = yes;
+
+        IKCONFIG = yes;
+        IKCONFIG_PROC = yes;
+        # Compile with headers
+        IKHEADERS = yes;
+
+        SLUB_DEBUG = yes;
+        DEBUG_MEMORY_INIT = yes;
+        KASAN = yes;
+
+        # FRAME_WARN - warn at build time for stack frames larger tahn this.
+
+        MAGIC_SYSRQ = yes;
+
+        LOCALVERSION = freeform localVersion;
+
+        LOCK_STAT = yes;
+        PROVE_LOCKING = yes;
+
+        FTRACE = yes;
+        STACKTRACE = yes;
+        IRQSOFF_TRACER = yes;
+
+        KGDB = yes;
+        UBSAN = yes;
+        BUG_ON_DATA_CORRUPTION = yes;
+        SCHED_STACK_END_CHECK = yes;
+        UNWINDER_FRAME_POINTER = yes;
+        "64BIT" = yes;
+        SMP = yes;
+        IA32_EMULATION = yes;
+        PREEMPT_DYNAMIC = yes;
+
+        # initramfs/initrd support
+        BLK_DEV_INITRD = yes;
+
+        PRINTK = yes;
+        PRINTK_TIME = yes;
+        EARLY_PRINTK = yes;
+
+        # Support elf and #! scripts
+        BINFMT_ELF = yes;
+        BINFMT_SCRIPT = yes;
+
+        # Create a tmpfs/ramfs early at bootup.
+        DEVTMPFS = yes;
+        DEVTMPFS_MOUNT = yes;
+
+        TTY = yes;
+        SERIAL_8250 = yes;
+        SERIAL_8250_CONSOLE = yes;
+
+        PROC_FS = yes;
+        SYSFS = yes;
+
+        MODULES = yes;
+        MODULE_UNLOAD = yes;
+
+        # FW_LOADER = yes;
+
+        ##
+        SYSVIPC = yes;
+        NET = yes;
+        PACKET = yes;
+        INET = yes;
+        NETDEVICES = yes;
+        NET_CORE = yes;
+        ETHERNET = yes;
+        PCI = yes;
+        NET_VENDOR_INTEL = yes;
+        E1000 = yes;
+        UNIX = yes;
+
+        EXPERT = yes;
+        TMPFS = yes;
+        MEMFD_CREATE = yes;
+
+        PID_NS = yes;
+
+        SECCOMP = yes;
+
+        ## BPF
+        DEBUG_INFO_DWARF4 = yes;
+        DEBUG_INFO_BTF = yes;
+        BPF_SYSCALL = yes;
+        BPF_JIT = yes;
+        FUNCTION_TRACER = yes;
+        # Enable kprobes and kallsyms: https://www.kernel.org/doc/html/latest/trace/kprobes.html#configuring-kprobes
+        # Debug FS is be enabled (done above) to show registered kprobes in /sys/kernel/debug: https://www.kernel.org/doc/html/latest/trace/kprobes.html#the-kprobes-debugfs-interface
+        KPROBES = yes;
+        KALLSYMS_ALL = yes;
+      } // lib.optionalAttrs enableGdb {
+        DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT = yes;
+        GDB_SCRIPTS = yes;
+      };
+
+    # Flags that get passed to generate-config.pl
+    generateConfigFlags = {
+      # Ignores any config errors (eg unused config options)
+      ignoreConfigErrors = false;
+      # Build every available module
+      autoModules = false;
+      preferBuiltin = false;
+    };
+  };
+}
