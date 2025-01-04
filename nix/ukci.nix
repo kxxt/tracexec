1b7290b95d246afbcf3bf4e2ee2592518d61d5cf
Levi Zim <rsworktech@outlook.com>
2025-01-04 14:18:18 +0800

    disable 6.6LTS in CI
    Ref #55

--- nix/ukci.nix
+++ nix/ukci.nix
@@ -1,53 +1,65 @@
 localFlake:
 
-{ lib, config, self, inputs, ... }:
 {
-  perSystem = { self', system, pkgs, ... }: {
-    packages =
-      let
-        sources = [
-          {
-            name = "6.1lts";
-            tag = "6.1.123";
-            source = "mirror";
-            test_exe = "tracexec_no_rcu_kfuncs";
-            sha256 = "sha256-M6es158EuHmcQohFctwu23WxzVhLHTLUaNHbEQJOs7w=";
-          }
-          {
-            name = "6.6lts";
-            tag = "6.6.69";
-            source = "mirror";
-            test_exe = "tracexec";
-            sha256 = "sha256-nGMFVnt12ZUUzebrneOZc/PVyFenW9nc38pXBB+NTzQ=";
-          }
-          {
-            name = "6.12";
-            tag = "6.12.8";
-            source = "mirror";
-            test_exe = "tracexec";
-            sha256 = "sha256-IpHaBlygS3Fcie5QNirsPwIadBS8lj8bVnNmgsgSKXk=";
-          }
-          {
-            name = "6.13rc";
-            tag = "v6.13-rc5";
-            version = "6.13.0-rc5";
-            source = "linus";
-            test_exe = "tracexec";
-            sha256 = "sha256-I55oKtG547JdR4bqDOqNmRn0WCtogOIE2rLX29rgGHo=";
-          }
-        ];
-        nixpkgs = localFlake.nixpkgs;
-        configureKernel = pkgs.callPackage ./kernel-configure.nix { };
-        buildKernel = pkgs.callPackage ./kernel-build.nix { };
-        kernelNixConfig = source: pkgs.callPackage ./kernel-source.nix source;
-        kernels = map
-          (source:
+  lib,
+  config,
+  self,
+  inputs,
+  ...
+}:
+{
+  perSystem =
+    {
+      self',
+      system,
+      pkgs,
+      ...
+    }:
+    {
+      packages =
+        let
+          sources = [
+            {
+              name = "6.1lts";
+              tag = "6.1.123";
+              source = "mirror";
+              test_exe = "tracexec_no_rcu_kfuncs";
+              sha256 = "sha256-M6es158EuHmcQohFctwu23WxzVhLHTLUaNHbEQJOs7w=";
+            }
+            # {
+            #   name = "6.6lts";
+            #   tag = "6.6.69";
+            #   source = "mirror";
+            #   test_exe = "tracexec";
+            #   sha256 = "sha256-nGMFVnt12ZUUzebrneOZc/PVyFenW9nc38pXBB+NTzQ=";
+            # }
+            {
+              name = "6.12";
+              tag = "6.12.8";
+              source = "mirror";
+              test_exe = "tracexec";
+              sha256 = "sha256-IpHaBlygS3Fcie5QNirsPwIadBS8lj8bVnNmgsgSKXk=";
+            }
+            {
+              name = "6.13rc";
+              tag = "v6.13-rc5";
+              version = "6.13.0-rc5";
+              source = "linus";
+              test_exe = "tracexec";
+              sha256 = "sha256-I55oKtG547JdR4bqDOqNmRn0WCtogOIE2rLX29rgGHo=";
+            }
+          ];
+          nixpkgs = localFlake.nixpkgs;
+          configureKernel = pkgs.callPackage ./kernel-configure.nix { };
+          buildKernel = pkgs.callPackage ./kernel-build.nix { };
+          kernelNixConfig = source: pkgs.callPackage ./kernel-source.nix source;
+          kernels = map (
+            source:
             let
               config = kernelNixConfig source;
               inherit (config) kernelArgs kernelConfig;
               configfile = configureKernel {
-                inherit
-                  (kernelConfig)
+                inherit (kernelConfig)
                   generateConfigFlags
                   structuredExtraConfig
                   ;
@@ -56,8 +68,7 @@ localFlake:
               linuxDev = pkgs.linuxPackagesFor kernelDrv;
               kernel = linuxDev.kernel;
               kernelDrv = buildKernel {
-                inherit
-                  (kernelArgs)
+                inherit (kernelArgs)
                   src
                   modDirVersion
                   version
@@ -79,83 +90,87 @@ localFlake:
                 };
                 storePaths = [ pkgs.foot.terminfo ];
               };
-            })
-          sources;
-      in
-      rec {
-        run-qemu =
-          let
-            shellCases = lib.concatMapStrings
-              ({ name, kernel, initramfs, ... }: ''
-                ${name})
-                  kernel="${kernel}"
-                  initrd="${initramfs}"
-                ;;
-              '')
-              kernels;
-          in
-          pkgs.writeScriptBin "run-qemu" ''
+            }
+          ) sources;
+        in
+        rec {
+          run-qemu =
+            let
+              shellCases = lib.concatMapStrings (
+                {
+                  name,
+                  kernel,
+                  initramfs,
+                  ...
+                }:
+                ''
+                  ${name})
+                    kernel="${kernel}"
+                    initrd="${initramfs}"
+                  ;;
+                ''
+              ) kernels;
+            in
+            pkgs.writeScriptBin "run-qemu" ''
+              #!/usr/bin/env sh
+
+              case "$1" in
+                ${shellCases}
+                *)
+                  echo "Invalid argument!"
+                  exit 1
+              esac
+
+              sudo qemu-system-x86_64 \
+                -enable-kvm \
+                -m 2G \
+                -smp cores=4 \
+                -kernel "$kernel"/bzImage \
+                -initrd "$initrd"/initrd.gz \
+                -device e1000,netdev=net0 \
+                -netdev user,id=net0,hostfwd=::10022-:22 \
+                -nographic -append "console=ttyS0"
+            '';
+          test-qemu = pkgs.writeScriptBin "test-qemu" ''
             #!/usr/bin/env sh
-
-            case "$1" in
-              ${shellCases}
-              *)
-                echo "Invalid argument!"
-                exit 1
-            esac
-
-            sudo qemu-system-x86_64 \
-              -enable-kvm \
-              -m 2G \
-              -smp cores=4 \
-              -kernel "$kernel"/bzImage \
-              -initrd "$initrd"/initrd.gz \
-              -device e1000,netdev=net0 \
-              -netdev user,id=net0,hostfwd=::10022-:22 \
-              -nographic -append "console=ttyS0"
-          '';
-        test-qemu = pkgs.writeScriptBin "test-qemu" ''
-          #!/usr/bin/env sh
-          ssh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@127.0.0.1 -p 10022"
-
-          # Wait for the qemu virtual machine to start...
-          for i in $(seq 1 12); do
-            [ $i -gt 1 ] && sleep 5;
-            $ssh true && break;
-          done;
-
-          # Show uname
-          $ssh uname -a
-          # Try to load eBPF module:
-          $ssh "$1" ebpf log -- ls
-          exit $?
-        '';
-
-        ukci =
-          let
-            platforms = lib.concatMapStringsSep " "
-              ({ name, test_exe, ... }: "${name}:${test_exe}")
-              kernels;
-          in
-          pkgs.writeScriptBin "ukci" ''
-            #!/usr/bin/env bash
-
-            set -e
-
-            if ! [[ "$(whoami)" == root ]]; then
-              sudo "$0"
-              exit $?
-            fi
-
             ssh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@127.0.0.1 -p 10022"
-            for platform in ${platforms}; do
-              IFS=: read -r kernel test_exe <<< "$platform"
-              ${run-qemu}/bin/run-qemu "$kernel" &
-              ${test-qemu}/bin/test-qemu "$test_exe"
-              $ssh poweroff -f || true
-              wait < <(jobs -p)
+
+            # Wait for the qemu virtual machine to start...
+            for i in $(seq 1 12); do
+              [ $i -gt 1 ] && sleep 5;
+              $ssh true && break;
             done;
+
+            # Show uname
+            $ssh uname -a
+            # Try to load eBPF module:
+            $ssh "$1" ebpf log -- ls
+            exit $?
           '';
-      };
-  };
+
+          ukci =
+            let
+              platforms = lib.concatMapStringsSep " " ({ name, test_exe, ... }: "${name}:${test_exe}") kernels;
+            in
+            pkgs.writeScriptBin "ukci" ''
+              #!/usr/bin/env bash
+
+              set -e
+
+              if ! [[ "$(whoami)" == root ]]; then
+                sudo "$0"
+                exit $?
+              fi
+
+              ssh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@127.0.0.1 -p 10022"
+              for platform in ${platforms}; do
+                IFS=: read -r kernel test_exe <<< "$platform"
+                ${run-qemu}/bin/run-qemu "$kernel" &
+                ${test-qemu}/bin/test-qemu "$test_exe"
+                $ssh poweroff -f || true
+                wait < <(jobs -p)
+              done;
+            '';
+        };
+    };
 }
