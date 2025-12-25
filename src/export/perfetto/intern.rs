9af0f47feecba2e2d135fd02fa488a4ca418235b
Levi Zim <rsworktech@outlook.com>
2025-12-25 22:41:29 +0800

    perfetto: intern strings
    The trace generated when building tracexec in release mode reduced
    from 7MB to 5.1MB with this change.
    I would expect larger builds(e.g. kernel, chromium) to benefit more.

--- src/export/perfetto/intern.rs
+++ src/export/perfetto/intern.rs
@@ -1,8 +1,11 @@
+use std::num::NonZeroUsize;
+
 use perfetto_trace_proto::{
-  DebugAnnotation, DebugAnnotationName,
+  DebugAnnotation, DebugAnnotationName, EventName, InternedString,
   debug_annotation::{NameField, Value},
 };
 use strum::{EnumIter, IntoEnumIterator, IntoStaticStr};
+
 
 #[repr(u64)]
 #[derive(Debug, Clone, Copy, IntoStaticStr, EnumIter)]
@@ -39,6 +42,14 @@ impl DebugAnnotationInternId {
     }
   }
 
+  pub fn with_interned_string(self, value: InternedId) -> DebugAnnotation {
+    DebugAnnotation {
+      value: Some(Value::StringValueIid(value)),
+      name_field: Some(NameField::NameIid(self as _)),
+      ..Default::default()
+    }
+  }
+
   pub fn with_array(self, value: Vec<DebugAnnotation>) -> DebugAnnotation {
     DebugAnnotation {
       array_values: value,
@@ -53,5 +64,84 @@ impl DebugAnnotationInternId {
       name_field: Some(NameField::NameIid(self as _)),
       ..Default::default()
     }
+  }
+}
+
+type InternedId = u64;
+
+/// A value that should be included in the intern table of the trace packet
+pub struct InternedValue {
+  pub iid: InternedId,
+  pub value: String,
+}
+
+impl From<InternedValue> for InternedString {
+  fn from(value: InternedValue) -> Self {
+    Self {
+      iid: Some(value.iid),
+      str: Some(value.value.into_bytes()),
+    }
+  }
+}
+
+impl From<InternedValue> for EventName {
+  fn from(value: InternedValue) -> Self {
+    Self {
+      iid: Some(value.iid),
+      name: Some(value.value),
+    }
+  }
+}
+
+pub struct ValueInterner {
+  /// The iid counter
+  iid: InternedId,
+  /// The cache
+  cache: lru::LruCache<String, InternedId, hashbrown::DefaultHashBuilder>,
+}
+
+impl ValueInterner {
+  pub fn new(max_cap: NonZeroUsize) -> Self {
+    Self {
+      iid: 1,
+      cache: lru::LruCache::new(max_cap),
+    }
+  }
+
+  /// Try to intern a string, if already interned, the iid is returned.
+  /// Otherwise we intern it and return the value to be inserted into intern table
+  pub fn intern(&mut self, msg: &str) -> Result<InternedId, InternedValue> {
+    if let Some(v) = self.cache.get(msg) {
+      Ok(*v)
+    } else {
+      let s = msg.to_owned();
+      let iid = self.iid;
+      self.iid += 1;
+      // Unfortunately we must clone the string for inserting it into the intern table.
+      self.cache.put(s.clone(), iid);
+      Err(InternedValue { iid, value: s })
+    }
+  }
+
+  pub fn intern_with(
+    &mut self,
+    msg: &str,
+    table: &mut Vec<impl From<InternedValue>>,
+  ) -> InternedId {
+    match self.intern(msg) {
+      Ok(iid) => iid,
+      Err(value) => {
+        let iid = value.iid;
+        table.push(value.into());
+        iid
+      }
+    }
+  }
+}
+
+pub fn da_interned_string(iid: InternedId) -> DebugAnnotation {
+  DebugAnnotation {
+    value: Some(Value::StringValueIid(iid)),
+    ..Default::default()
   }
 }
