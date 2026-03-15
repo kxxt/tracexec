f06d0f5580e1bc02de548c556183ddbd34ad8762
Levi Zim <rsworktech@outlook.com>
2026-03-15 21:04:16 +0800

    nix: integrate treefmt-nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     crane.url = "github:ipetkov/crane";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
   };
 
   outputs =
@@ -17,12 +19,17 @@
       let
         inherit (flake-parts-lib) importApply;
         tracexec.default = importApply ./nix/tracexec.nix { inherit crane; };
-        ukci.default = importApply ./nix/ukci.nix { inherit nixpkgs; inherit crane; };
+        ukci.default = importApply ./nix/ukci.nix {
+          inherit nixpkgs;
+          inherit crane;
+        };
       in
       {
         imports = [
           tracexec.default
           ukci.default
+          inputs.treefmt-nix.flakeModule
+          inputs.flake-root.flakeModule
         ];
         flake = { };
         systems = [
@@ -42,14 +49,18 @@
           let
             defaultShell = pkgs.mkShell {
               name = "Development Shell";
-              packages = with pkgs; [
-                strace
-                nixpkgs-fmt
-                self'.packages.ukci
-                self'.packages.run-qemu
-                self'.packages.test-qemu
-              ];
-              shellHook = ''export TRACEXEC_LOGLEVEL=debug'';
+              packages =
+                with pkgs;
+                [
+                  strace
+                  nixfmt
+                  config.treefmt.build.wrapper
+                  self'.packages.ukci
+                  self'.packages.run-qemu
+                  self'.packages.test-qemu
+                ]
+                ++ builtins.attrValues config.treefmt.build.programs;
+              shellHook = "export TRACEXEC_LOGLEVEL=debug";
             };
           in
           {
@@ -57,19 +68,28 @@
             devShells.default = defaultShell;
             devShells.extended = pkgs.mkShell {
               inputsFrom = [ defaultShell ];
-              packages = lib.optionals (system != "aarch64-linux") [ 
-                self'.packages.ukci-aarch64
-                self'.packages.run-qemu-aarch64
-                self'.packages.test-qemu-aarch64
-              ] ++ lib.optionals (system != "x86_64-linux") [ 
-                self'.packages.ukci-x86_64
-                self'.packages.run-qemu-x86_64
-                self'.packages.test-qemu-x86_64
-              ] ++ lib.optionals (system != "riscv64-linux") [ 
-                self'.packages.ukci-riscv64
-                self'.packages.run-qemu-riscv64
-                self'.packages.test-qemu-riscv64
-              ];
+              packages =
+                lib.optionals (system != "aarch64-linux") [
+                  self'.packages.ukci-aarch64
+                  self'.packages.run-qemu-aarch64
+                  self'.packages.test-qemu-aarch64
+                ]
+                ++ lib.optionals (system != "x86_64-linux") [
+                  self'.packages.ukci-x86_64
+                  self'.packages.run-qemu-x86_64
+                  self'.packages.test-qemu-x86_64
+                ]
+                ++ lib.optionals (system != "riscv64-linux") [
+                  self'.packages.ukci-riscv64
+                  self'.packages.run-qemu-riscv64
+                  self'.packages.test-qemu-riscv64
+                ];
+            };
+
+            treefmt.config = {
+              inherit (config.flake-root) projectRootFile;
+              # formats .nix files
+              programs.nixfmt.enable = true;
             };
           };
       }
