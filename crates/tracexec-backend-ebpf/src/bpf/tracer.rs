072f1e8bdc053ab511f833e01189d1913e16dffa
Levi Zim <rsworktech@outlook.com>
2026-03-15 11:01:45 +0800

    refactor, bpf: move feature probe to a separate mod

--- crates/tracexec-backend-ebpf/src/bpf/tracer.rs
+++ crates/tracexec-backend-ebpf/src/bpf/tracer.rs
@@ -4,7 +4,6 @@ use std::{
     HashMap,
     HashSet,
   },
-  env,
   ffi::OsStr,
   io::stdin,
   iter::repeat_n,
@@ -23,7 +22,6 @@ use std::{
   time::Duration,
 };
 
-use cfg_if::cfg_if;
 use chrono::Local;
 use color_eyre::Section;
 use enumflags2::{
@@ -67,7 +65,6 @@ use nix::{
     tcsetpgrp,
   },
 };
-use procfs::ConfigSetting;
 use tokio::sync::mpsc::UnboundedSender;
 use tracexec_core::{
   cli::args::ModifierArgs,
@@ -136,7 +133,13 @@ use super::{
   },
   utf8_lossy_cow_from_bytes_with_nul,
 };
-use crate::bpf::tracer::private::Sealed;
+use crate::{
+  bpf::tracer::private::Sealed,
+  probe::{
+    kernel_have_ftrace_with_direct_calls,
+    kernel_have_syscall_wrappers,
+  },
+};
 
 pub struct EbpfTracer {
   user: Option<User>,
@@ -743,57 +746,4 @@ fn attach_kprobes_without_syscall_wrappers(skel: &mut TracexecSystemSkel) -> lib
       .attach_kprobe(true, "__se_sys_execveat")?,
   );
   Ok(())
-}
-
-fn kernel_have_syscall_wrappers(
-  #[allow(unused)] kconfig: Option<&HashMap<String, ConfigSetting>>,
-) -> bool {
-  // arm64 and x86_64 both have syscall wrappers long before 5.17
-  cfg_if! {
-   if #[cfg(target_arch = "riscv64")] {
-      // https://github.com/torvalds/linux/commit/b21cdb9523e5561b97fd534dbb75d132c5c938ff
-      kconfig
-        .map(|configs| configs.contains_key("CONFIG_ARCH_HAS_SYSCALL_WRAPPER"))
-        .unwrap_or_default() ||
-        tracexec_core::is_current_kernel_ge((6, 6)).unwrap_or_default()
-    } else {
-      true
-    }
-  }
-}
-
-fn kernel_have_ftrace_with_direct_calls(kconfig: Option<&HashMap<String, ConfigSetting>>) -> bool {
-  // First, check special env `TRACEXEC_USE_KPROBE`
-  if env::var("TRACEXEC_USE_KPROBE")
-    .inspect_err(|e| warn!("Failed to read env TRACEXEC_USE_KPROBE: {e}"))
-    .map(|v| !v.is_empty())
-    .unwrap_or_default()
-  {
-    return false;
-  }
-  env::var("TRACEXEC_USE_FENTRY")
-    .inspect_err(|e| warn!("Failed to read env TRACEXEC_USE_FENTRY: {e}"))
-    .map(|v| !v.is_empty())
-    .unwrap_or_default() ||
-  // Then, we try to read kernel config
-  kconfig
-    .map(|configs| configs.contains_key("CONFIG_DYNAMIC_FTRACE_WITH_DIRECT_CALLS"))
-    .unwrap_or_default() ||
-  // Finally, we try to decide based on kernel version
-  {
-    cfg_if! {
-      if #[cfg(target_arch = "x86_64")] {
-        // We support linux >= 5.17, which all have this feature
-        true
-      } else if #[cfg(target_arch = "aarch64")] {
-        // https://github.com/torvalds/linux/commit/2aa6ac03516d078cf0c35aaa273b5cd11ea9734c
-        tracexec_core::is_current_kernel_ge((6, 4)).unwrap_or_default()
-      } else if #[cfg(target_arch = "riscv64")] {
-        // https://github.com/torvalds/linux/commit/b21cdb9523e5561b97fd534dbb75d132c5c938ff
-        tracexec_core::is_current_kernel_ge((6, 16)).unwrap_or_default()
-      } else {
-          compile_error!("unsupported architecture");
-      }
-    }
-  }
 }
