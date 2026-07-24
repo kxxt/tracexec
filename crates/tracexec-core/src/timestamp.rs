c11629101b015b24125c15c6677114c1e7e3c68b
Levi Zim <i@kxxt.dev>
2026-07-24 21:58:30 +0700

    refactor(core): make inline_timestamp_format more idomatic

--- crates/tracexec-core/src/timestamp.rs
+++ crates/tracexec-core/src/timestamp.rs
@@ -15,6 +15,13 @@ use nutype::nutype;
 )]
 pub struct TimestampFormat(String);
 
+impl Default for TimestampFormat {
+  #[allow(clippy::unwrap_used)]
+  fn default() -> Self {
+    Self::try_new("%H:%M:%S").unwrap()
+  }
+}
+
 fn validate_strftime(fmt: &str) -> Result<(), Cow<'static, str>> {
   if fmt.contains("\n") {
     return Err("inline timestamp format string should not contain newline(s)".into());
@@ -31,14 +38,11 @@ pub fn ts_from_boot_ns(boot_ns: u64) -> Timestamp {
 static BOOT_TIME: LazyLock<u64> = LazyLock::new(|| {
   let content = std::fs::read_to_string("/proc/stat").expect("Failed to read /proc/stat");
   for line in content.lines() {
-    if line.starts_with("btime") {
-      return line
-        .split(' ')
-        .nth(1)
-        .unwrap()
-        .parse::<u64>()
-        .expect("Failed to parse btime in /proc/stat")
-        * 1_000_000_000;
+    if let Some(boot_time) = line
+      .strip_prefix("btime ")
+      .and_then(|value| value.parse::<u64>().ok())
+    {
+      return boot_time * 1_000_000_000;
     }
   }
   panic!("btime is not available in /proc/stat. Am I running on Linux?")
